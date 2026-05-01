34878ad37f7e371f70632bf1a990dca12e36999b
angrypig555 <brny@brny.dev>
2026-05-01 20:12:37 +0100

    add hypotenuse support

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+
+pub fn calc_hypotenuse(a: f64, b: f64) -> f64 {
+    (a.powi(2) + b.powi(2)).sqrt()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+use std::io::Write;
+
+use triangulizer::calc_hypotenuse;
+
+fn readln() -> String {
+    let mut input = String::new();
+    io::stdout().flush().unwrap();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Entered string incorrect");
+
+    input.trim().to_string()
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Triangulizer");
+    println!("Please choose option:\n1. Calculate hypotenuse\n");
+    let choice = readln();
+    if choice == "1" {
+        print!("Please enter side 1: ");
+        let a: f64 = readln().parse().expect("Please enter a number");
+        print!("Please enter side 2: ");
+        let b: f64 = readln().parse().expect("Please enter a number");
+        let hypotenuse = calc_hypotenuse(a, b);
+        println!("Your hypotenuse is: {}", hypotenuse);
+        println!("Brought to you by the pythagorean theorem");
+    }
 }
