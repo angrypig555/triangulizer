dc256d856084c58d899d57dd2499d645b8d51c19
angrypig555 <brny@brny.dev>
2026-05-02 10:12:24 +0100

    add feature to calculate otherside based on hypotenuse

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,7 @@
 pub fn calc_hypotenuse(a: f64, b: f64) -> f64 {
     (a.powi(2) + b.powi(2)).sqrt()
 }
+
+pub fn calc_other_side_with_hypotenuse(a: f64, hyp: f64) -> f64 {
+    (hyp.powi(2) - a.powi(2)).sqrt()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,89 @@
 use std::io;
 use std::io::Write;
-
-use triangulizer::calc_hypotenuse;
+use std::process;
+use triangulizer::{calc_hypotenuse, calc_other_side_with_hypotenuse};
 
 fn readln() -> String {
     let mut input = String::new();
     io::stdout().flush().unwrap();
     io::stdin()
         .read_line(&mut input)
-        .expect("Entered string incorrect");
+        .expect("Entered string is incorrect");
 
     input.trim().to_string()
 }
 
 
 fn main() {
+    let triangle_empty = r"
+            ##                                              
+            # #                                             
+            #  #                                            
+            #   #                                           
+            #    #                                          
+            #     #                                         
+            #      #                                        
+            #       #                                       
+            #        #                                      
+       a    #         #    c                                
+            #          #                                    
+            #           #                                   
+            #            #                                  
+            #             #                                 
+            #              #                                
+            #               #                               
+            #################                               
+                                                            
+                    b                                ";
+    let triangle_hypotenuse = r"
+            ##                                              
+            # #                                             
+            #  #                                            
+            #   #                                           
+            #    #                                          
+            #     #                                         
+            #      #                                        
+            #       #                                       
+            #        #                                      
+       a    #         #    (c) <-- CALCULATING THIS SIDE                                
+            #          #                                    
+            #           #                                   
+            #            #                                  
+            #             #                                 
+            #              #                                
+            #               #                               
+            #################                               
+                                                            
+                    b                                ";
     println!("Triangulizer");
-    println!("Please choose option:\n1. Calculate hypotenuse\n");
-    let choice = readln();
-    if choice == "1" {
-        print!("Please enter side 1: ");
-        let a: f64 = readln().parse().expect("Please enter a number");
-        print!("Please enter side 2: ");
-        let b: f64 = readln().parse().expect("Please enter a number");
-        let hypotenuse = calc_hypotenuse(a, b);
-        println!("Your hypotenuse is: {}", hypotenuse);
-        println!("Brought to you by the pythagorean theorem");
+    println!("{}", triangle_empty);
+    loop {
+        println!("Please choose option:\n1. Calculate hypotenuse\n2. Calculate side from hypotenuse and one other side\n3. Exit");
+        let choice = readln();
+        if choice == "1" {
+            println!("{}", triangle_hypotenuse);
+            println!("not to scale");
+            print!("Please enter side 1: ");
+            let a: f64 = readln().parse().expect("Please enter a number");
+            print!("Please enter side 2: ");
+            let b: f64 = readln().parse().expect("Please enter a number");
+            let hypotenuse = calc_hypotenuse(a, b);
+            println!("Your hypotenuse is: {}", hypotenuse);
+        } else if choice == "2" {
+            print!("Please enter hypotenuse length: ");
+            let hyp: f64 = readln().parse().expect("Please enter a number");
+            print!("Enter other side's length: ");
+            let o: f64 = readln().parse().expect("Please enter a number");
+            if hyp < o {
+                println!("Other side cannot be larger than hypotenuse!");
+                continue;
+            }
+            let missing_side = calc_other_side_with_hypotenuse(o, hyp);
+            println!("Your other side is: {}", missing_side);
+        } else if choice == "3" {
+            process::exit(0);
+        } else {
+            println!("Invalid choice");
+        }
     }
 }
